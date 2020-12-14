e31b8ff4546449fd8654990876c03a47ec861211
Luca <bembenek.luca@gmail.com>
2020-12-14 08:01:38 +0100

    add user to mapper test

--- src/__tests__/mapper.test.ts
+++ src/__tests__/mapper.test.ts
@@ -1,5 +1,8 @@
+import { User } from './user';
 import { RDFMapper } from '../mapper';
 
 test('RDFMapper', () => {
+    const user = new User('1234', 'Hello there');
+    console.log(RDFMapper.serialize(user).sparql());
     expect(1).toEqual(1);
 });
