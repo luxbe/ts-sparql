6afdc7f185708d807fcc1549b001dbeea0e522dc
Luca <bembenek.luca@gmail.com>
2020-12-16 11:42:58 +0100

    add Entity decorator

--- src/decorators/entity.ts
+++ src/decorators/entity.ts
@@ -1,39 +1,14 @@
-import { Storage } from '../storage/storage';
+import { Storage, IPrefixes } from '../storage/storage';
 
-export interface IEntity {
-    __rdfid__: {
-        prefix: string;
-        key: string;
-    };
-    __prefixes__: { [key: string]: string };
-}
-
-export const instanceOfEntity = (object: any): object is IEntity => {
-    return (
-        object !== undefined &&
-        object.__rdfid__ !== undefined &&
-        object.__rdfprefixes__ !== undefined
-    );
-};
-
-export function Entity(name: string, prefixes?: IEntity['__prefixes__']) {
-    if (Storage.global.names.includes(name))
-        throw new Error(`${name} is already defined`);
-
-    Storage.global.names.push(name);
-
+export function Entity(name?: string, prefixes?: IPrefixes) {
     return <T extends new (...args: any[]) => {}>(constructor: T) => {
-        const get = (): IEntity['__prefixes__'] => {
-            return prefixes || {};
-        };
+        name ||= constructor.name.toLowerCase();
+        prefixes ||= {};
 
-        const set = (_prefixes: IEntity['__prefixes__']) => {
-            prefixes = _prefixes;
-        };
+        if (Storage.global.names.includes(name))
+            throw new Error(`${name} is already defined`);
 
-        Object.defineProperty(constructor.prototype, '__prefixes__', {
-            get,
-            set,
-        });
+        Storage.global.names.push(name);
+        Storage.global.prefixes[name] = prefixes;
     };
 }
