820adaa730515511e73a0d09c3732c7ded51ebe5
Luca <bembenek.luca@gmail.com>
2020-12-16 21:03:32 +0100

    add save method

--- src/tssparql/tssparql.ts
+++ src/tssparql/tssparql.ts
@@ -1,35 +1,90 @@
-import { Storage } from '../storage/storage';
+import { isValidRegex } from '../helper';
+import { IPrefixes, IStorage, Storage } from '../storage/storage';
+import { Relation, SparqlMapper } from './sparql.mapper';
 
 interface TsSparqlOptions {
     url?: string;
     host?: string;
     port?: number;
+    defaultVoc?: string;
     repository: string;
 }
 
 export class TsSparql {
     url: string;
-    storage: Storage;
+    storage: IStorage;
+    mapper: SparqlMapper;
+    defaultVoc: string;
+    private _prefixes: IPrefixes;
+    private _graph?: string;
 
     static init(options: TsSparqlOptions) {
         if (!options.url) {
             if (!options.host && !options.url)
-                throw new Error(`Expected either 'url' or 'host' and 'port'`);
+                throw new Error(`Expected either 'host' and 'port' or 'url'`);
             options.url = `${options.host}:${options.port}/`;
         }
+        if (options.defaultVoc && !isValidRegex(options.defaultVoc, '/:#'))
+            throw new Error(`'${options.defaultVoc}' does not match RegExp`);
+
         if (options.url.charAt(options.url.length - 1) !== '/')
             options.url += '/';
 
         options.url += `repositories/${options.repository}/statements`;
-        return new TsSparql(options.url, Storage.global);
+        return new TsSparql(options.url, Storage.global, options.defaultVoc);
     }
 
-    constructor(url: string, storage: Storage) {
+    constructor(url: string, storage: IStorage, defaultVoc: string = '') {
         this.url = url;
         this.storage = storage;
+        this.mapper = new SparqlMapper();
+        this.defaultVoc = defaultVoc;
+        this._prefixes = {};
     }
 
-    save<T>(entity: T) {
-        console.log(typeof entity);
+    prefixes(prefixes: IPrefixes) {
+        this._prefixes = prefixes;
+        return this;
+    }
+
+    graph(graph: string) {
+        this._graph = graph;
+        return this;
+    }
+
+    save<T extends { [key: string]: any }>(entity: T) {
+        // get name
+        const name = entity.constructor.name.toLowerCase();
+        if (!this.storage.names.includes(name))
+            throw new Error('Entity was not defined');
+
+        // get id
+        const id_key = this.storage.ids[name];
+        if (!id_key) throw new Error('Entity does not have an Id');
+        const id = entity[id_key];
+
+        // get prefixes
+        const _prefixes = this.storage.prefixes[name];
+
+        // get properties
+        const property_keys = this.storage.properties[name];
+        if (!property_keys.length)
+            throw new Error('Entity does not have any properties');
+
+        const properties: Relation[] = [];
+        property_keys.forEach((p) =>
+            properties.push({
+                predicate: (p.prefix || this.defaultVoc) + ':' + p.key,
+                object: entity[p.key],
+                literal: p.literal,
+            }),
+        );
+
+        const sparql = this.mapper
+            .subject(name + ':' + 'id')
+            .assign(properties)
+            .sparql('INSERT', this._graph, _prefixes, this._prefixes);
+
+        return this;
     }
 }
