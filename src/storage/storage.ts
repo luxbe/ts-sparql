c81d12fb79e8d51b62bbdf488c60ac2239ae8343
Luca <bembenek.luca@gmail.com>
2020-12-16 11:43:24 +0100

    add prefixes, ids and properties to storage

--- src/storage/storage.ts
+++ src/storage/storage.ts
@@ -1,10 +1,26 @@
+export interface IPrefixes {
+    [key: string]: string;
+}
+
 interface IStorage {
     names: string[];
+    prefixes: {
+        [key: string]: IPrefixes;
+    };
+    ids: {
+        [key: string]: string;
+    };
+    properties: {
+        [key: string]: string[];
+    };
 }
 
 export class Storage {
     private static _global: IStorage = {
         names: [],
+        prefixes: {},
+        ids: {},
+        properties: {},
     };
 
     static get global() {
