ce354e6f3328ca008438e5043d43d0c0fcf0e0f3
Luca <bembenek.luca@gmail.com>
2020-12-16 21:03:06 +0100

    add tssparql tests

--- src/__tests__/tssparql.test.ts
+++ src/__tests__/tssparql.test.ts
@@ -1,11 +1,33 @@
 import { TsSparql } from '..';
-let tsSparql = TsSparql.init({
-    url: 'http://localhost:7200/',
-    repository: 'example',
-});
+import { User } from './user';
 
 it('should store the url', () => {
+    let tsSparql = TsSparql.init({
+        url: 'http://localhost:7200/',
+        repository: 'example',
+    });
+
     expect(tsSparql.url).toEqual(
         'http://localhost:7200/repositories/example/statements',
     );
+});
+
+it('should build the url with host and port', () => {
+    let tsSparql = TsSparql.init({
+        host: 'http://localhost',
+        port: 7200,
+        repository: 'example',
+    });
+
+    expect(tsSparql.url).toEqual(
+        'http://localhost:7200/repositories/example/statements',
+    );
+});
+
+it('should throw error when no url, host and port provided', () => {
+    expect(() => {
+        TsSparql.init({
+            repository: 'example',
+        });
+    }).toThrowError(`Expected either 'host' and 'port' or 'url'`);
 });
