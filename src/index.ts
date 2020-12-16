d6dbde541f3759fc2ca6378b9d5588243bd58a4f
Luca <bembenek.luca@gmail.com>
2020-12-16 21:16:46 +0100

    remove RDFMapper

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,6 @@
 import { Entity } from './decorators/entity';
 import { Id } from './decorators/id';
 import { Property } from './decorators/property';
-import { RDFMapper } from './mapper';
 import { TsSparql } from './tssparql/tssparql';
 
-export { Id, Property, RDFMapper, Entity, TsSparql };
+export { Id, Property, Entity, TsSparql };
