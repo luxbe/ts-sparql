d84b74f0730227d38bb749d2476b049bad0d2dc7
Luca <bembenek.luca@gmail.com>
2020-12-16 21:00:59 +0100

    add prefix and literal to properties

--- src/decorators/property.ts
+++ src/decorators/property.ts
@@ -1,9 +1,12 @@
+import { isValidRegex } from '../helper';
 import { Storage } from '../storage/storage';
 
-export function Property() {
+export function Property(prefix?: string, literal: boolean = true) {
+    if (prefix && !isValidRegex(prefix))
+        throw new Error(`'${prefix}' does not match RegExp in class`);
     return (target: object, key: string) => {
         const name = target.constructor.name.toLowerCase();
         Storage.global.properties[name] ||= [];
-        Storage.global.properties[name].push(key);
+        Storage.global.properties[name].push({ key, prefix, literal });
     };
 }
