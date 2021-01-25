1b684c5799a71bb1db97e65cc866e049c740ef68
luxbe <bembenek.luca@gmail.com>
2021-01-25 22:48:02 +0100

    add EntityMapper to map Result to Entity

---
+++ src/mapper/mapper.entity.ts
@@ -0,0 +1,40 @@
+import { DataMapper } from '.';
+import { Binding, Type } from '../interfaces';
+import { Metadata } from '../metadata';
+
+export class EntityMapper {
+    private static _instance: EntityMapper;
+    private dataMapper: DataMapper;
+
+    private constructor() {
+        this.dataMapper = DataMapper.global;
+    }
+
+    public static get global() {
+        if (!EntityMapper._instance) {
+            EntityMapper._instance = new EntityMapper();
+        }
+        return EntityMapper._instance;
+    }
+
+    map<T>(type: Type<T>, binding: Binding): T {
+        const entity = Object.create(type.prototype);
+
+        const { idKey, properties, type: _t } = Metadata.global
+            .entityType(type)
+            .getMetadata();
+
+        entity[idKey] = binding[idKey]!.value.substr(_t.toString().length - 2);
+
+        properties.forEach((p) => {
+            const b = binding[p.key];
+            if (b == undefined) return;
+            entity[p.key] = this.dataMapper.mapFromString(
+                binding[p.key].value,
+                binding[p.key].datatype || 'String',
+            );
+        });
+
+        return entity;
+    }
+}
