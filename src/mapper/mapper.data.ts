d644d96cd23e604a8408266efcc82090f9e04942
luxbe <bembenek.luca@gmail.com>
2021-01-25 22:48:30 +0100

    add DataMapper to Map single data entries

--- src/mapper/mapper.data.ts
+++ src/mapper/mapper.data.ts
@@ -1,22 +1,20 @@
 import { Type } from '../interfaces';
+import { Metadata } from '../metadata';
 import { XSD } from '../namespaces';
-
-export type DataMap = (value: any) => string;
 
 export class DataMapper {
     private static _instance: DataMapper;
-    private _mappers: {
-        [key: string]: DataMap;
+    private _metadata = Metadata.global;
+
+    private _objectMappers: {
+        [key: string]: (value: string) => any;
+    } = {};
+    private _stringMappers: {
+        [key: string]: (value: any) => string;
     } = {
-        String: (value) => {
-            return `"${value}"`;
-        },
-        Date: (value) => {
-            return `"${(value as Date).toISOString()}"^^<${XSD.DATE}>`;
-        },
-        Boolean: (value) => {
-            return `"${value as Boolean}"^^<${XSD.BOOLEAN}>`;
-        },
+        String: (value) => `"${value}"`,
+        Date: (value) => `"${(value as Date).toISOString()}"^^<${XSD.DATE}>`,
+        Boolean: (value) => `"${value as Boolean}"^^<${XSD.BOOLEAN}>`,
     };
 
     private constructor() {}
@@ -28,27 +26,56 @@ export class DataMapper {
         return DataMapper._instance;
     }
 
-    public static register<T>(
+    public static registerObjectMap<T>(
         datatype: string | Type<T>,
-        func: (value: any) => string,
+        map: (value: any) => string,
         override: boolean = false,
     ) {
         if (typeof datatype != 'string') datatype = datatype.name;
 
         if (
-            Object.keys(DataMapper.global._mappers).includes(datatype) &&
+            Object.keys(DataMapper.global._objectMappers).includes(datatype) &&
             !override
         )
             throw new Error(`Mapper for ${datatype} is already defined`);
 
-        DataMapper.global._mappers[datatype] = func;
+        DataMapper.global._objectMappers[datatype] = map;
     }
-
-    map<T>(value: any, datatype: string | Type<T>): string {
+    public static registerStringMap<T>(
+        datatype: string | Type<T>,
+        map: (value: string) => any,
+        override: boolean = false,
+    ) {
         if (typeof datatype != 'string') datatype = datatype.name;
 
-        if (!Object.keys(this._mappers).includes(datatype))
-            return value.toString();
-        return this._mappers[datatype](value);
+        if (
+            Object.keys(DataMapper.global._stringMappers).includes(datatype) &&
+            !override
+        )
+            throw new Error(`Mapper for ${datatype} is already defined`);
+
+        DataMapper.global._stringMappers[datatype] = map;
+    }
+
+    mapToString<T>(value: any, datatype: Type<T> | string): string {
+        if (typeof datatype != 'string') datatype = datatype.name;
+
+        if (!Object.keys(this._stringMappers).includes(datatype)) {
+            if (!this._metadata.storage.entities.includes(datatype))
+                return value.toString();
+            const { type, idKey } = this._metadata
+                .entityType(datatype)
+                .getMetadata();
+            type.setPredicate(type.predicate + value[idKey]);
+
+            return type.toString();
+        }
+
+        return this._stringMappers[datatype](value);
+    }
+
+    mapFromString(value: string, datatype: string): any {
+        if (!Object.keys(this._objectMappers).includes(datatype)) return value;
+        return this._objectMappers[datatype](value);
     }
 }
