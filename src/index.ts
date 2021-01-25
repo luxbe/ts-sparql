c2fbe0fd8a130f59b5e497f48f9525c970f3565d
luxbe <bembenek.luca@gmail.com>
2021-01-25 22:42:27 +0100

    add Mappers to export

--- src/index.ts
+++ src/index.ts
@@ -2,5 +2,6 @@ import { TsSparql } from './tssparql';
 export * from './namespaces';
 export * from './decorators';
 export * from './iri';
+export { DataMapper, EntityMapper } from './mapper';
 
 export default TsSparql;
