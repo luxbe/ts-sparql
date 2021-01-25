6fe9f8839353a4c749f4eecbdaa0a1eb4a7951ca
luxbe <bembenek.luca@gmail.com>
2021-01-25 22:49:02 +0100

    export mappers

--- src/mapper/index.ts
+++ src/mapper/index.ts
@@ -1,1 +1,5 @@
-export * from './mapper.sparql';
+import { DataMapper } from './mapper.data';
+import { EntityMapper } from './mapper.entity';
+import { SPARQLMapper } from './mapper.sparql';
+
+export { SPARQLMapper, DataMapper, EntityMapper };
