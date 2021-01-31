b141e715460c047a64fcbd37c87dbc1dc2ec2f1b
luxbe <bembenek.luca@gmail.com>
2021-01-31 09:22:50 +0100

    add exports

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,8 @@
 import { TsSparql } from './tssparql';
-export * from './namespaces';
 export * from './decorators';
 export * from './iri';
 export { DataMapper, EntityMapper } from './mapper';
+export * from './namespaces';
+export { PrefixManager } from './prefixManager';
 
 export default TsSparql;
