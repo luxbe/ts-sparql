c3c4f423d1251df78ef7c735fce4aab70c26e40a
luxbe <bembenek.luca@gmail.com>
2021-01-31 09:21:41 +0100

    add RDF and RDFS Namespaces

---
+++ src/namespaces/rdf.namespace.ts
@@ -0,0 +1,16 @@
+export default class RDF {
+    /**
+     * The namespace of the Vocabulary
+     */
+    static NAMESPACE = 'http://www.w3.org/1999/02/22-rdf-syntax-ns#';
+
+    /**
+     * A common prefix for the Vocabulary
+     */
+    static PREFIX = 'rdf';
+
+    /**
+     * The resource is an instance of a class
+     */
+    static TYPE = RDF.NAMESPACE + 'type';
+}
---
+++ src/namespaces/rdfs.namespace.ts
@@ -0,0 +1,16 @@
+export default class RDFS {
+    /**
+     * The namespace of the Vocabulary
+     */
+    static NAMESPACE = 'http://www.w3.org/2000/01/rdf-schema#';
+
+    /**
+     * A common prefix for the Vocabulary
+     */
+    static PREFIX = 'rdfs';
+
+    /**
+     * The resource is an instance of a class
+     */
+    static CLASS = RDFS.NAMESPACE + 'Class';
+}
