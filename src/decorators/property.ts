7a2e03234cf840f304a1ce5b44d71660587073a2
luxbe <bembenek.luca@gmail.com>
2021-02-03 20:32:05 +0100

    check for Array property

--- src/decorators/property.ts
+++ src/decorators/property.ts
@@ -1,15 +1,17 @@
 import 'reflect-metadata';
-import { Property as IProperty, PropertyMetadata } from '../interfaces';
+import { Property as IProperty, PropertyMetadata, Type } from '../interfaces';
 import { Iri } from '../iri';
 
 interface Options {
     optional?: boolean;
-    entity?: string;
+    entity?: string | Type<any>;
 }
 
 export function Property(iri: Iri | string, options: Options = {}) {
     return (target: object, key: string) => {
         if (typeof iri === 'string') iri = new Iri(iri);
+        if (options.entity !== undefined && typeof options.entity !== 'string')
+            options.entity = options.entity.name;
 
         const properties: IProperty[] =
             Reflect.getMetadata(
@@ -28,7 +30,8 @@ export function Property(iri: Iri | string, options: Options = {}) {
             iri,
             optional: options.optional || false,
             entity: options.entity,
-            datatype,
+            datatype: datatype === 'Array' ? undefined : datatype,
+            isArray: datatype === 'Array',
         });
 
         Reflect.defineMetadata(
